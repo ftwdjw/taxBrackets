c07fc36d70729bb01bee682f3770e5619237f9c0
ftwdjw <ftwdjw@gmail.com>
2018-02-28 11:38:37 -0600

    works

--- tax/Program.cs
+++ tax/Program.cs
@@ -9,13 +9,14 @@ namespace tax
 
         public static void Main(string[] args)
         {
-            var income = 100000.0;
+            var income = 120000.0;
             var tax = 0.0;
             double[] taxRate = new[] { 10.0 , 12.0, 22.0, 24.0, 32.0, 35.0, 37.0 };
             double[] brackets = new[] { 0.0 , 19050.0, 77400.0, 165000.0, 315000.0,
                 400000.0, 600000.0, 1000000.0 };
             
-            string [] sum  = new string [taxRate.Length];
+            string [] outputArray  = new string [taxRate.Length+10];//used to print file with WriteAllLines
+
             double[] cumulativeTax = new double[taxRate.Length];
             bool FindTaxBracket = true;
 
@@ -36,7 +37,8 @@ namespace tax
                 }
 
                 
-                sum[i] = taxRate[i].ToString() + ","
+                outputArray[i] = taxRate[i].ToString() + ","
+
                                    + brackets[i].ToString() + ","
                                    + cumulativeTax[i].ToString();;
                 
@@ -54,14 +56,43 @@ namespace tax
                 i++;
             }
 
-            tax = cumulativeTax[saveBracket] + (income - brackets[saveBracket]) * taxRate[saveBracket];
+            double deltaToMin = (income - brackets[saveBracket]);
+
+            tax =  (deltaToMin * taxRate[saveBracket] * .01) + cumulativeTax[saveBracket];
+
+            //write to console
 
             Console.WriteLine("income="+income);
-            Console.WriteLine("tax Bracket=" + brackets[saveBracket]+"to"+brackets[saveBracket+1]);
+
+            Console.WriteLine("tax Bracket=" + brackets[saveBracket]+" to "+brackets[saveBracket+1]);
+
             Console.WriteLine("tax rate=" + taxRate[saveBracket]);
+
+            Console.WriteLine("income delta to min bracket=" + deltaToMin);
+
+            Console.WriteLine("cumulative Tax =" + cumulativeTax[saveBracket]);
+
             Console.WriteLine("tax =" + tax);
 
-            File.WriteAllLines("tax.csv",sum);
+            //write to output csv file for exel in bin directory
+
+
+                
+                outputArray[taxRate.Length + 1] = "income" + "," + income.ToString() ;
+
+            outputArray[taxRate.Length + 2] = "tax bracket" + ","
+                + brackets[saveBracket].ToString() + "to" + brackets[saveBracket + 1].ToString();
+
+            outputArray[taxRate.Length + 3] = "tax Rate" + "," + taxRate[saveBracket].ToString();
+
+            outputArray[taxRate.Length + 4] = "tax " + "," + tax.ToString();
+            
+
+
+
+
+
+            File.WriteAllLines("tax.csv",outputArray);
             Console.WriteLine("have written file");
         }
     }
